1709895a6103ce1bf279ee2455ab865bee736566
jwc20 <choi.jae100@gmail.com>
2025-07-01 22:53:14 +0900

    add classes

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
 See also:
 
 - [ROCK, PAPER, SCISSORS, ETC - TOPICS IN THE THEORY OF REGULAR TOURNAMENTS by ETHAN AKIN](https://arxiv.org/pdf/1806.11241)
+
+
+
+Normal-form Matrix:
+
+- https://youtu.be/-1GDMXoMdaY?si=01Bx5GFGrJhCi-dX
+
+- https://www.researchgate.net/publication/10810903_Dressing_the_mind_properly_for_the_game
--- rps.py
+++ rps.py
@@ -1,28 +1,46 @@
 
+"""
+
+1. Players make their actions 
+    - actions depend on the number of actions
+
+2. Game rules are set
+    - number of actions (action_count) 
+    - number of rounds
+    - maximum number of players 
+"""
 
 
 
 
 
+class Game:
+    def __init__(self, players, action_count) -> None:
+        self.players = players
+        self.action_count = action_count
+        
+
+
+class Player:
+    def __init__(self, name, action) -> None:
+        self.name = name 
+        self.action = action
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-def main():
-    pass 
 
 
 
 
 if __name__ == "__main__":
-    main()
+    pass
+    # actions in {rock, paper, scissors, ...} or {0, 1, 2, ...}
+
+    # p1 = Player("jon", 1)
+    # p2 = Player("don", 2)
+    # p3 = Player("dave", 0)
+
+    # players = list(p1, p2, p3)
+
+    # game = Game(players, action_count)
