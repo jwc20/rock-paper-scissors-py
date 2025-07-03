b1627e599a676c22a41a0a236781a9f6a471ae62
jwc20 <choi.jae100@gmail.com>
2025-07-03 13:45:27 +0900

    add game state class

--- rps.py
+++ rps.py
@@ -18,6 +18,14 @@ class Player:
         return self._action
 
 
+class GameState:
+    def __init__(self):
+        self.round_number = 0
+        self.history = []
+        self.winner = None
+        self.is_finished = False
+
+
 class Game:
     def __init__(self, players: List[Player], action_count: int) -> None:
         if action_count % 2 == 0:
